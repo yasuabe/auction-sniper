ac19ff009a2aec4dade6063a678d6d3658fbe92f
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-18 23:25:07 +0900

    11.2: MainWindow の実装追加.

--- auction-sniper3/src/auctionsniper/ui/MainWindow.java
+++ auction-sniper3/src/auctionsniper/ui/MainWindow.java
@@ -2,7 +2,14 @@ package auctionsniper.ui;
 
 import javax.swing.JFrame;
 
+import auctionsniper.Main;
+
 @SuppressWarnings("serial")
 public class MainWindow extends JFrame {
-
+	public MainWindow() {
+		super("Auction Sniper");
+		setName(Main.MAIN_WINDOW_NAME);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 }
