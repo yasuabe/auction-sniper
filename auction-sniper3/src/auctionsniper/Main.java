790e85f7e289dfdf309f316ce4387b5b0ea4ded2
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-18 23:14:54 +0900

    11.1: AuctionSniperDriver とビルドを通すための空実装を追加。

--- auction-sniper3/src/auctionsniper/Main.java
+++ auction-sniper3/src/auctionsniper/Main.java
@@ -3,6 +3,10 @@ package auctionsniper;
 public class Main {
 	public static final String STATUS_JOINING = "Joining";
 	public static final String STATUS_LOST = "Lost";
+	// TODO 空実装
+	public static final String MAIN_WINDOW_NAME = null;
+	// TODO 空実装
+	public static final String SNIPER_STATUS_NAME = null;
 	public static void main(String... args) {
 		// TODO Auto-generated method stub
 		
--- auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -1,19 +1,29 @@
 package test.endtoend.auctionsniper;
 
-public class AuctionSniperDriver {
+import org.hamcrest.Matcher;
+
+import auctionsniper.Main;
+
+import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JFrameDriver;
+import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.gesture.GesturePerformer;
+
+import static org.hamcrest.CoreMatchers.*;
+
+@SuppressWarnings("unchecked")
+public class AuctionSniperDriver extends JFrameDriver {
 
 	public AuctionSniperDriver(int timeoutMillis) {
-		// TODO Auto-generated constructor stub
+		super(new GesturePerformer(),
+				JFrameDriver.topLevelFrame(
+						named(Main.MAIN_WINDOW_NAME),
+						showingOnScreen()),
+						new AWTEventQueueProber(timeoutMillis, 100));
 	}
 
 	public void showsSniperStatus(String statusText) {
-		// TODO Auto-generated method stub
-		
+		new JLabelDriver(this, named(Main.SNIPER_STATUS_NAME))
+		.hasText((Matcher<String>) equalTo(statusText));
 	}
-
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
