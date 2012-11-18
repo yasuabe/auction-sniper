a6722f98193bdd356484e0b0100c46decb36a007
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-18 23:29:16 +0900

    11.2: MainWindow にラベルコンポーネントを追加した.

--- auction-sniper3/src/auctionsniper/ui/MainWindow.java
+++ auction-sniper3/src/auctionsniper/ui/MainWindow.java
@@ -1,15 +1,29 @@
 package auctionsniper.ui;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
 
 import auctionsniper.Main;
 
 @SuppressWarnings("serial")
 public class MainWindow extends JFrame {
+	public static final String SNIPER_STATUS_NAME = "sniper status";
+	private final JLabel sniperStatus = createLabel1(Main.STATUS_JOINING);
 	public MainWindow() {
 		super("Auction Sniper");
 		setName(Main.MAIN_WINDOW_NAME);
+		add(sniperStatus);
+		pack();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+	}
+	private JLabel createLabel1(String initialText) {
+		JLabel result = new JLabel(initialText);
+		result.setName(SNIPER_STATUS_NAME);
+		result.setBorder(new LineBorder(Color.BLACK));
+		return result;
 	}
 }
