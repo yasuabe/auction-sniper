dff004c107c1d9a6f578f1e1634e19a1e749a847
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-18 22:56:48 +0900

    10.2: ビルドを通すための空実装を Eclipse に自動生成させて10章終わり

---
+++ auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/ApplicationRunner.java
@@ -0,0 +1,20 @@
+package test.endtoend.auctionsniper;
+
+public class ApplicationRunner {
+
+	public void startBiddingIn(FakeAuctionServer auction) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void showsSniperHasLostAuction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/FakeAuctionServer.java
@@ -0,0 +1,29 @@
+package test.endtoend.auctionsniper;
+
+public class FakeAuctionServer {
+
+	public FakeAuctionServer(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void startSellingItem() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void hasReceivedJoinRequestFromSniper() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void announceClosed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
