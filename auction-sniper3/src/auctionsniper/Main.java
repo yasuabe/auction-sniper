66398f84a8ab62a0b375774fe2ee0659d3ec04a0
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-18 23:23:40 +0900

    11.2: Main の実装と MainWndow の空実装の追加

--- auction-sniper3/src/auctionsniper/Main.java
+++ auction-sniper3/src/auctionsniper/Main.java
@@ -1,15 +1,29 @@
 package auctionsniper;
 
+import javax.swing.SwingUtilities;
+
+import auctionsniper.ui.MainWindow;
+
 public class Main {
 	public static final String STATUS_JOINING = "Joining";
 	public static final String STATUS_LOST = "Lost";
-	// TODO 空実装
-	public static final String MAIN_WINDOW_NAME = null;
-	// TODO 空実装
-	public static final String SNIPER_STATUS_NAME = null;
-	public static void main(String... args) {
-		// TODO Auto-generated method stub
-		
+	public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
+	public static final String SNIPER_STATUS_NAME = "sniper status";
+	
+	private MainWindow ui;
+	
+	public Main() throws Exception {
+		startUserInterface();
 	}
-
+	private void startUserInterface() throws Exception {
+		SwingUtilities.invokeAndWait(new Runnable() {
+			@Override
+			public void run() {
+				ui = new MainWindow();
+			}
+		});
+	}
+	public static void main(String... args) throws Exception {
+		Main main = new Main();
+	}
 }
---
+++ auction-sniper3/src/auctionsniper/ui/MainWindow.java
@@ -0,0 +1,8 @@
+package auctionsniper.ui;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class MainWindow extends JFrame {
+
+}
