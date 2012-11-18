926ec8bc2cca2565f443b1511173a3bedf443db2
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-18 23:18:26 +0900

    11.1: FakeAuctionServer に実装追加。

--- auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/FakeAuctionServer.java
+++ auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/FakeAuctionServer.java
@@ -1,35 +1,75 @@
 package test.endtoend.auctionsniper;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ChatManagerListener;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
 public class FakeAuctionServer {
+	class SingleMessageListener implements MessageListener {
+		private final ArrayBlockingQueue<Message> messages = new ArrayBlockingQueue<>(1);
+
+		@Override
+		public void processMessage(Chat chat, Message message) {
+			messages .add(message);
+		}
+
+		public void receivedAMessage() throws InterruptedException {
+			assertThat("Message", messages.poll(5, TimeUnit.SECONDS), is(notNullValue()));
+		}
+		
+	}
+
 	public static final String XMPP_HOSTNAME = "localhost";
+	public static final String ITEM_ID_AS_LOGIN = "auction-%s";
+	public static final String AUCTION_PASSWORD = "auction";
+	public static final String AUCTION_RESOURCE = "Auction";
 
-	public FakeAuctionServer(String string) {
-		// TODO Auto-generated constructor stub
+	private final String itemId;
+	private final XMPPConnection connection;
+	private final SingleMessageListener messageListener = new SingleMessageListener();
+
+	private Chat currentChat;
+
+	public FakeAuctionServer(String itemId) {
+		this.itemId = itemId;
+		this.connection = new XMPPConnection(XMPP_HOSTNAME);
 	}
 
-	public void startSellingItem() {
-		// TODO Auto-generated method stub
-		
+	public void startSellingItem() throws XMPPException {
+		connection.connect();
+		connection.login(String.format(ITEM_ID_AS_LOGIN, itemId),
+				AUCTION_PASSWORD, AUCTION_RESOURCE);
+		connection.getChatManager().addChatListener(new ChatManagerListener() {
+			@Override
+			public void chatCreated(Chat chat, boolean createdLocally) {
+				currentChat = chat;
+				chat.addMessageListener(messageListener);
+			}
+		});
 	}
 
-	public void hasReceivedJoinRequestFromSniper() {
-		// TODO Auto-generated method stub
-		
+	public void hasReceivedJoinRequestFromSniper() throws InterruptedException {
+		messageListener.receivedAMessage();
 	}
 
-	public void announceClosed() {
-		// TODO Auto-generated method stub
-		
+	public void announceClosed() throws XMPPException {
+		currentChat.sendMessage(new Message());
 	}
 
 	public void stop() {
-		// TODO Auto-generated method stub
-		
+		connection.disconnect();
 	}
 
 	public String getItemId() {
-		// TODO Auto-generated method stub
-		return null;
+		return itemId;
 	}
-
 }
