2765a185f23b310b2f2c7d93ba6b066b4cbc6119
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-19 23:45:52 +0900

    notifiesAuctionCloseWhenCloseMessageReceived()にエクスペクテーション追記

---
+++ auction-sniper3/src/auctionsniper/xmpp/AuctionEventListener.java
@@ -0,0 +1,7 @@
+package auctionsniper.xmpp;
+
+public interface AuctionEventListener {
+
+	void auctionClosed();
+
+}
--- auction-sniper3/test/unit/test/auctionsniper/AuctionMessageTranslatorTest.java
+++ auction-sniper3/test/unit/test/auctionsniper/AuctionMessageTranslatorTest.java
@@ -2,19 +2,32 @@ package test.auctionsniper;
 
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.packet.Message;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import auctionsniper.xmpp.AuctionEventListener;
 import auctionsniper.xmpp.AuctionMessageTranslator;
 
+@RunWith(JMock.class)
 public class AuctionMessageTranslatorTest {
+
 	public static final Chat UNUSED_CHAT = null;
+	
+	private final Mockery context = new Mockery();
+	private final AuctionEventListener listener = context.mock(AuctionEventListener.class);
+	
 	private final AuctionMessageTranslator translator = new AuctionMessageTranslator();
 	
 	@Test public void notifiesAuctionCloseWhenCloseMessageReceived() {
+		context.checking(new Expectations() {{
+			oneOf(listener).auctionClosed();
+		}});
 		Message message = new Message();
 		message.setBody("SOLVersion: 1.1; Event: CLOSE;");
 		
 		translator.processMessage(UNUSED_CHAT, message);
 	}
-	
 }
