93a7ba63b19ae8fb787be25d6d00d9f827ef904b
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-20 00:02:22 +0900

    12.4: processMessage() と announceClosed()の実装

--- auction-sniper3/src/auctionsniper/xmpp/AuctionMessageTranslator.java
+++ auction-sniper3/src/auctionsniper/xmpp/AuctionMessageTranslator.java
@@ -1,5 +1,8 @@
 package auctionsniper.xmpp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.packet.Message;
@@ -11,7 +14,23 @@ public class AuctionMessageTranslator implements MessageListener {
 		this.listener = listener;
 	}
 
-	@Override public void processMessage(Chat arg0, Message arg1) {
-		this.listener.auctionClosed();
+	@Override public void processMessage(Chat chat, Message message) {
+		Map<String, String> event = unpackEventFrom(message);
+		String type = event.get("Event");
+		if ("CLOSE".equals(type)) {
+			listener.auctionClosed();
+		} else if ("PRICE".equals(type)) {
+			listener.currentPrice(Integer.parseInt(event.get("CurrentPrice")),
+					Integer.parseInt(event.get("Increment")));
+		}
+	}
+
+	private Map<String, String> unpackEventFrom(Message message) {
+		Map<String, String> event = new HashMap<>();
+		for (String element: message.getBody().split(";")) {
+			String[] pair = element.split(":");
+			event.put(pair[0].trim(), pair[1].trim());
+		}
+		return event;
 	}
 }
