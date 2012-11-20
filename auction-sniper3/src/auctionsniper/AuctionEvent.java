5c4bbe5a261bdf4cca05fefe83dce2932208c051
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-20 22:26:33 +0900

    13.3: AuctionEvent をちゃんと実装

--- auction-sniper3/src/auctionsniper/AuctionEvent.java
+++ auction-sniper3/src/auctionsniper/AuctionEvent.java
@@ -1,25 +1,35 @@
 package auctionsniper;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AuctionEvent {
+	private final Map<String, String> fields = new HashMap<String, String>();
 
-	public String type() {
-		// TODO Auto-generated method stub
-		return null;
+	public String type() { return get("Event"); }
+	public int currentPrice() { return getInt("CurrentPrice"); }
+	public int increment() { return getInt("Increment"); }
+
+	private int getInt(String fieldName) {
+		return Integer.parseInt(get(fieldName));
+	}
+	
+	private String get(String fieldName) {
+		return fields.get(fieldName);
 	}
 
-	public static AuctionEvent from(String body) {
-		// TODO Auto-generated method stub
-		return null;
+	public static AuctionEvent from(String messageBody) {
+		AuctionEvent event = new AuctionEvent();
+		for (String field : fieldsIn(messageBody)) {
+			event.addField(field);
+		}
+		return event;
 	}
-
-	public int currentPrice() {
-		// TODO Auto-generated method stub
-		return 0;
+	private void addField(String field) {
+		String[] pair = field.split(":");
+		fields.put(pair[0].trim(), pair[1].trim());
 	}
-
-	public int increment() {
-		// TODO Auto-generated method stub
-		return 0;
+	static String[] fieldsIn(String messageBody) {
+		return messageBody.split(";");
 	}
-
 }
