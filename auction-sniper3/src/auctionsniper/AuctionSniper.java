e5b5305a18ff24868c7d5eb4887bee0f5ef95a05
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-20 21:06:46 +0900

    13.1: AuctionSniper とそのユニットテストの空実装を追加

---
+++ auction-sniper3/src/auctionsniper/AuctionSniper.java
@@ -0,0 +1,19 @@
+package auctionsniper;
+
+import auctionsniper.xmpp.AuctionEventListener;
+
+public class AuctionSniper implements AuctionEventListener {
+
+	@Override
+	public void auctionClosed() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void currentPrice(int i, int j) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ auction-sniper3/test/unit/test/auctionsniper/AuctionSniperTest.java
@@ -0,0 +1,14 @@
+package test.auctionsniper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AuctionSniperTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
