fb1e5e1132d22c4199e5d98ccedcb2d608e36071
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-21 21:43:23 +0900

    14.5: エンドツーエンドまで緑に

--- auction-sniper3/src/auctionsniper/ui/SniperStateDisplayer.java
+++ auction-sniper3/src/auctionsniper/ui/SniperStateDisplayer.java
@@ -34,7 +34,6 @@ public class SniperStateDisplayer implements SniperListener {
 	}
 	@Override
 	public void sniperWon() {
-		// TODO Auto-generated method stub
-		
+		showStatus(MainWindow.STATUS_WON);
 	}
 }
