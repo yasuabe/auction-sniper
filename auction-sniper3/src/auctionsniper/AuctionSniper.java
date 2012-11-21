814a7e40aedbe3ec91f6ee3ae8e28ec90a8c7004
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-21 21:41:42 +0900

    14.5: 先のテストを緑に

--- auction-sniper3/src/auctionsniper/AuctionSniper.java
+++ auction-sniper3/src/auctionsniper/AuctionSniper.java
@@ -4,6 +4,7 @@ import auctionsniper.xmpp.AuctionEventListener;
 
 public class AuctionSniper implements AuctionEventListener {
 
+	private boolean isWinning  = false;
 	private final SniperListener sniperListener;
 	private final Auction auction;
 
@@ -14,19 +15,21 @@ public class AuctionSniper implements AuctionEventListener {
 
 	@Override
 	public void auctionClosed() {
-		sniperListener.sniperLost();
+		if (isWinning) {
+			sniperListener.sniperWon();
+		} else {
+			sniperListener.sniperLost();
+		}
 	}
 
 	@Override
 	public void currentPrice(int price, int increment, PriceSource priceSource) {
-		switch (priceSource) {
-		case FromSniper:
+		isWinning = priceSource == PriceSource.FromSniper;
+		if (isWinning) {
 			sniperListener.sniperWinning();
-			break;
-		case FromOtherBidder:
+		} else {
 			auction.bid(price + increment);
 			sniperListener.sniperBidding();
-			break;
 		}
 	}
 }
