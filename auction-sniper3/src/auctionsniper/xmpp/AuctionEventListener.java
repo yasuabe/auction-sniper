f2f878ad8f70f59a3d487292a8567bcf195a8907
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-21 20:15:23 +0900

    14.1: 列挙値 PriceSource の追加

--- auction-sniper3/src/auctionsniper/xmpp/AuctionEventListener.java
+++ auction-sniper3/src/auctionsniper/xmpp/AuctionEventListener.java
@@ -2,8 +2,10 @@ package auctionsniper.xmpp;
 
 public interface AuctionEventListener {
 
+	enum PriceSource {
+		FromSniper,
+		FromOtherBidder
+	}
 	void auctionClosed();
-
 	void currentPrice(int i, int j);
-
 }
