04a03b1422fffb3147bc5a9040e0fc5287204184
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-23 00:03:17 +0900

    15.1: E2E でラベルではなくテーブルを期待するように修正

--- auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -1,15 +1,12 @@
 package test.endtoend.auctionsniper;
 
-import org.hamcrest.Matcher;
-
+import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
 import auctionsniper.Main;
 
 import com.objogate.wl.swing.AWTEventQueueProber;
 import com.objogate.wl.swing.driver.JFrameDriver;
-import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.driver.JTableDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
-
-import static org.hamcrest.CoreMatchers.*;
 
 @SuppressWarnings("unchecked")
 public class AuctionSniperDriver extends JFrameDriver {
@@ -23,7 +20,6 @@ public class AuctionSniperDriver extends JFrameDriver {
 	}
 
 	public void showsSniperStatus(String statusText) {
-		new JLabelDriver(this, named(Main.SNIPER_STATUS_NAME))
-		.hasText((Matcher<String>) equalTo(statusText));
+		new JTableDriver(this).hasCell(withLabelText(statusText));
 	}
 }
