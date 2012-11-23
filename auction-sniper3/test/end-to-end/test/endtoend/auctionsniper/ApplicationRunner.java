dbbc897acd5d612762fbd336ee2cb4783f1f9920
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-23 21:45:24 +0900

    16.1: ApplicationRunner の各メソッドに auction パラメータを追加

--- auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/ApplicationRunner.java
+++ auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/ApplicationRunner.java
@@ -12,10 +12,8 @@ public class ApplicationRunner {
 	public static final String SNIPER_XMPP_ID = SNIPER_ID + "@" + XMPP_HOSTNAME + "/Auction";
 
 	private AuctionSniperDriver driver;
-	private String itemId;
 
 	public void startBiddingIn(final FakeAuctionServer auction) {
-		itemId = auction.getItemId();
 		Thread thread = new Thread("Test Application") {
 			@Override
 			public void run() {
@@ -35,24 +33,24 @@ public class ApplicationRunner {
 		driver.showsSniperStatus("", 0, 0, SnipersTableModel.textFor(SniperState.JOINING));
 	}
 
-	public void showsSniperHasLostAuction(int lastPrice, int lastBid) {
-		driver.showsSniperStatus(itemId, lastPrice, lastBid,
+	public void showsSniperHasLostAuction(FakeAuctionServer auction, int lastPrice, int lastBid) {
+		driver.showsSniperStatus(auction.getItemId(), lastPrice, lastBid,
 				SnipersTableModel.textFor(SniperState.LOST));
 	}
 
 	public void stop() {
 		if (driver != null) driver.dispose();
 	}
-	public void hasShownSniperIsBidding(int lastPrice, int lastBid) {
-		driver.showsSniperStatus(itemId, lastPrice, lastBid,
+	public void hasShownSniperIsBidding(FakeAuctionServer auction, int lastPrice, int lastBid) {
+		driver.showsSniperStatus(auction.getItemId(), lastPrice, lastBid,
 				SnipersTableModel.textFor(SniperState.BIDDING));
 	}
-	public void hasShownSniperIsWinning(int winningBid) {
-		driver.showsSniperStatus(itemId, winningBid, winningBid,
+	public void hasShownSniperIsWinning(FakeAuctionServer auction, int winningBid) {
+		driver.showsSniperStatus(auction.getItemId(), winningBid, winningBid,
 				SnipersTableModel.textFor(SniperState.WINNING));
 	}
-	public void showsSniperHasWonAuction(int lastPrice) {
-		driver.showsSniperStatus(itemId, lastPrice, lastPrice,
+	public void showsSniperHasWonAuction(FakeAuctionServer auction, int lastPrice) {
+		driver.showsSniperStatus(auction.getItemId(), lastPrice, lastPrice,
 				SnipersTableModel.textFor(SniperState.WON));
 	}
 }
--- auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
+++ auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -14,7 +14,7 @@ public class AuctionSniperEndToEndTest {
 		auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
 		
 		auction.announceClosed();
-		application.showsSniperHasLostAuction(0, 0);
+		application.showsSniperHasLostAuction(auction, 0, 0);
 	}
 	@Test public void sniperMakesAHigherBidButLoses() throws Exception {
 		auction.startSellingItem();
@@ -24,11 +24,11 @@ public class AuctionSniperEndToEndTest {
 		
 		auction.reportPrice(1000, 98, "other bidder");
 		
-		application.hasShownSniperIsBidding(1000, 1098);
+		application.hasShownSniperIsBidding(auction, 1000, 1098);
 		auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
 		
 		auction.announceClosed();
-		application.showsSniperHasLostAuction(1000, 1098);
+		application.showsSniperHasLostAuction(auction, 1000, 1098);
 	}
 	@Test public void sniperWinsAnAuctionByBiddingHigher() throws Exception {
 		auction.startSellingItem();
@@ -37,15 +37,15 @@ public class AuctionSniperEndToEndTest {
 		auction.hasReceivedJoinRequestFrom(ApplicationRunner.SNIPER_XMPP_ID);
 		
 		auction.reportPrice(1000, 98, "other bidder");
-		application.hasShownSniperIsBidding(1000, 1098);
+		application.hasShownSniperIsBidding(auction, 1000, 1098);
 
 		auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
 
 		auction.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
-		application.hasShownSniperIsWinning(1098);
+		application.hasShownSniperIsWinning(auction, 1098);
 		
 		auction.announceClosed();
-		application.showsSniperHasWonAuction(1098);
+		application.showsSniperHasWonAuction(auction, 1098);
 	}
 	@After public void stopAuction() {
 		auction.stop();
