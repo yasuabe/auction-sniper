b665ca95c802306793c0b505c8a72cc9eeb19e97
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-23 23:32:32 +0900

    16.2: 行を追加していたコードを削除して期待通りにE2Eが赤

--- auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/ApplicationRunner.java
+++ auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/ApplicationRunner.java
@@ -15,19 +15,19 @@ public class ApplicationRunner {
 	private AuctionSniperDriver driver;
 
 	public void startBiddingIn(final FakeAuctionServer... auctions) {
-		startSniper(auctions);
+		startSniper();
 		for (FakeAuctionServer auction: auctions) {
 			final String itemId = auction.getItemId();
 			driver.startBiddingFor(itemId);
 			driver.showsSniperStatus(itemId, 0, 0, textFor(SniperState.JOINING));
 		}
 	}
-	private void startSniper(final FakeAuctionServer... auctions) {
+	private void startSniper() {
 		Thread thread = new Thread("Test Application") {
 			@Override
 			public void run() {
 				try {
-					Main.main(arguments(auctions));
+					Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -39,17 +39,6 @@ public class ApplicationRunner {
 		driver = new AuctionSniperDriver(1000);
 		driver.hasTitle(MainWindow.APPLICATION_TITLE);
 		driver.hasColumnTitles();
-	}
-	private static String[] arguments(FakeAuctionServer... auctions) {
-		String[] arguments = new String[auctions.length + 3];
-		arguments[0] = XMPP_HOSTNAME;
-		arguments[1] = SNIPER_ID;
-		arguments[2] = SNIPER_PASSWORD;
-	
-		for (int i = 0; i < auctions.length; i++) {
-			arguments[i + 3] = auctions[i].getItemId();
-		}
-		return arguments;
 	}
 	public void showsSniperHasLostAuction(FakeAuctionServer auction, int lastPrice, int lastBid) {
 		driver.showsSniperStatus(auction.getItemId(), lastPrice, lastBid,
