ef7a7f074208e97fc6ccd890559b4bce8dc9d486
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-23 23:27:19 +0900

    16.2: makeControlsを追加

--- auction-sniper3/src/auctionsniper/ui/MainWindow.java
+++ auction-sniper3/src/auctionsniper/ui/MainWindow.java
@@ -2,10 +2,14 @@ package auctionsniper.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 
 import auctionsniper.Main;
 import auctionsniper.SniperSnapshot;
@@ -26,16 +30,31 @@ public class MainWindow extends JFrame {
 		this.snipers = snipers; 
 		
 		setName(Main.MAIN_WINDOW_NAME);
-		fillContentPane(makeSnipersTable());
+		fillContentPane(makeSnipersTable(), makeControls());
 		pack();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
-	private void fillContentPane(JTable snipersTable) {
+	private void fillContentPane(JTable snipersTable, JPanel controls) {
 		final Container contentPane = getContentPane();
 		contentPane.setLayout(new BorderLayout());
 		
+		contentPane.add(controls, BorderLayout.NORTH);
 		contentPane.add(new JScrollPane(snipersTable), BorderLayout.CENTER);
+	}
+	private JPanel makeControls() {
+		JPanel controls = new JPanel(new FlowLayout());
+		final JTextField itemIdField = new JTextField();
+		
+		itemIdField.setColumns(25);
+		itemIdField.setName(NEW_ITEM_ID_NAME);
+		controls.add(itemIdField);
+		
+		JButton joinAuctionButton = new JButton("Join Auction");
+		joinAuctionButton.setName(JOIN_BUTTON_NAME);
+		controls.add(joinAuctionButton);
+		
+		return controls;
 	}
 	private JTable makeSnipersTable() {
 		final JTable snipersTable = new JTable(snipers);
