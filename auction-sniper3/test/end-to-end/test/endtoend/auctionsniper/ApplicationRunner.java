235a9e34b254ada01735e64eecc9237ccf175eb3
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-23 21:16:17 +0900

    15.5: カラムタイトルをチェックして赤

--- auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/ApplicationRunner.java
+++ auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/ApplicationRunner.java
@@ -3,6 +3,7 @@ package test.endtoend.auctionsniper;
 import static test.endtoend.auctionsniper.FakeAuctionServer.XMPP_HOSTNAME;
 import auctionsniper.Main;
 import auctionsniper.SniperState;
+import auctionsniper.ui.MainWindow;
 import auctionsniper.ui.SnipersTableModel;
 
 public class ApplicationRunner {
@@ -29,6 +30,8 @@ public class ApplicationRunner {
 		thread.setDaemon(true);
 		thread.start();
 		driver = new AuctionSniperDriver(1000);
+		driver.hasTitle(MainWindow.APPLICATION_TITLE);
+		driver.hasColumnTitles();
 		driver.showsSniperStatus("", 0, 0, SnipersTableModel.textFor(SniperState.JOINING));
 	}
 
--- auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -4,10 +4,13 @@ import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
 import static com.objogate.wl.swing.matcher.IterableComponentsMatcher.matching;
 import static java.lang.String.valueOf;
 
+import javax.swing.table.JTableHeader;
+
 import auctionsniper.Main;
 import com.objogate.wl.swing.AWTEventQueueProber;
 import com.objogate.wl.swing.driver.JFrameDriver;
 import com.objogate.wl.swing.driver.JTableDriver;
+import com.objogate.wl.swing.driver.JTableHeaderDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
 @SuppressWarnings("unchecked")
@@ -26,5 +29,14 @@ public class AuctionSniperDriver extends JFrameDriver {
 		JTableDriver table = new JTableDriver(this);
 		table.hasRow(matching(withLabelText(itemId), withLabelText(valueOf(lastPrice)),
 				withLabelText(valueOf(lastBid)), withLabelText(statusText)));
+	}
+	public void hasColumnTitles() {
+		JTableHeaderDriver headers = new JTableHeaderDriver(
+				this, JTableHeader.class);
+		headers.hasHeaders(matching(
+				withLabelText("Item"),
+				withLabelText("Last Price"),
+				withLabelText("Last Bid"),
+				withLabelText("State")));
 	}
 }
