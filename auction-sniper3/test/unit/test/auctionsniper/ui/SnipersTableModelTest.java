50758d793b932e267c0f7a6c68d075f0d99c8a5a
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-23 18:51:57 +0900

    15.2: SnipersTableModelTest に各列の値が変更される事を確かめるテストコードを追加

--- auction-sniper3/test/unit/test/auctionsniper/ui/SnipersTableModelTest.java
+++ auction-sniper3/test/unit/test/auctionsniper/ui/SnipersTableModelTest.java
@@ -1,15 +1,22 @@
 package test.auctionsniper.ui;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
 
+import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 
+import org.hamcrest.Matcher;
+import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.junit.Before;
 import org.junit.Test;
 
+import auctionsniper.SniperState;
 import auctionsniper.ui.Column;
+import auctionsniper.ui.MainWindow;
 import auctionsniper.ui.SnipersTableModel;
 
 public class SnipersTableModelTest {
@@ -23,5 +30,25 @@ public class SnipersTableModelTest {
 	@Test
 	public void hasEnoughColumns() {
 	    assertThat(model.getColumnCount(), equalTo(Column.values().length)); 
+	}
+	@Test
+	public void setsSniperValuesInColumns() {
+		context.checking(new Expectations(){{
+			one(listener).tableChanged(with(aRowChangedEvent()));
+		}});
+		model.sniperStatusChanged(new SniperState("item id", 555, 666),
+				MainWindow.STATUS_BIDDING);
+		assertColumnEquals(Column.ITEM_IDENTIFIER, "item id");
+		assertColumnEquals(Column.LAST_PRICE, 555);
+		assertColumnEquals(Column.LAST_BID, 666);
+		assertColumnEquals(Column.SNIPER_STATE, MainWindow.STATUS_BIDDING);
+	}
+	private void assertColumnEquals(Column column, Object expected) {
+		final int rowIndex = 0;
+		final int columnIndex = column.ordinal();
+		assertEquals(expected, model.getValueAt(rowIndex, columnIndex));
+	}
+	private Matcher<TableModelEvent> aRowChangedEvent() {
+		return samePropertyValuesAs(new TableModelEvent(model, 0));
 	}
 }
