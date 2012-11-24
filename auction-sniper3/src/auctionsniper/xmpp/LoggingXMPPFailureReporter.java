cf4dd444c948c16a7e92fffb419b00838a72df5d
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-24 17:08:12 +0900

    19.5: LoggingXMPPFailureReporterの空実装とテストを追加

---
+++ auction-sniper3/src/auctionsniper/xmpp/LoggingXMPPFailureReporter.java
@@ -0,0 +1,17 @@
+package auctionsniper.xmpp;
+
+import java.util.logging.Logger;
+
+public class LoggingXMPPFailureReporter implements XMPPFailureReporter {
+
+	public LoggingXMPPFailureReporter(Logger logger) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void cannotTranslateMessage(String auctionId, String failedMessage,
+			Exception exception) {
+		// TODO Auto-generated method stub
+		
+	}
+}
