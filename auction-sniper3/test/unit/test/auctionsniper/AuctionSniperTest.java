e90581cbb56d967d644d0add4a27408d0dd384f8
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-24 16:14:50 +0900

    19.3: AuctionsniperTest に新テスト追加

--- auction-sniper3/test/unit/test/auctionsniper/AuctionSniperTest.java
+++ auction-sniper3/test/unit/test/auctionsniper/AuctionSniperTest.java
@@ -154,7 +154,28 @@ public class AuctionSniperTest {
 		sniper.currentPrice(price1, 25, PriceSource.FromOtherBidder);
 		sniper.currentPrice(price2, 25, PriceSource.FromOtherBidder);
 	}
-	
+	@Test
+	public void reportsFailedIfAuctionFailsWhenBidding() {
+		ignoringAuction();
+		allowingSniperBidding();
+
+		expectSniperToFailWhenItIs("bidding");
+
+		sniper.currentPrice(123, 45, PriceSource.FromOtherBidder);
+		sniper.auctionFailed();
+	}
+	private void ignoringAuction() {
+		context.checking(new Expectations() {{
+			ignoring(auction);
+		}});
+	}
+	private void expectSniperToFailWhenItIs(final String state) {
+		context.checking(new Expectations() {{
+			atLeast(1).of(sniperListener).sniperStateChanged(
+					new SniperSnapshot(ITEM_ID, 00, 0, SniperState.FAILED));
+			when(sniperState.is(state));
+		}});
+	}
 	private Matcher<SniperSnapshot> aSniperThatIs(final SniperState state) {
 		return new FeatureMatcher<SniperSnapshot, SniperState>(
 				equalTo(state), "sniper that is ", "was") {
