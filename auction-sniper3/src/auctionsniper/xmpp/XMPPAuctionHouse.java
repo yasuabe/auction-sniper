299324c782adc1638eb46842aa212dfc5c3071f1
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-24 17:20:46 +0900

    19.5: XMPPAuctionHouse のログ出力コード実装

--- auction-sniper3/src/auctionsniper/xmpp/XMPPAuctionHouse.java
+++ auction-sniper3/src/auctionsniper/xmpp/XMPPAuctionHouse.java
@@ -1,5 +1,10 @@
 package auctionsniper.xmpp;
 
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import org.apache.commons.io.FilenameUtils;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 
@@ -11,17 +16,18 @@ public class XMPPAuctionHouse implements AuctionHouse {
 
 	public static final String AUCTION_RESOURCE = "Auction";
 	public static final String LOG_FILE_NAME = "auction-sniper.log";
+	private static final String LOGGER_NAME = "auction-sniper";
 	private XMPPConnection connection;
-
+	private final XMPPFailureReporter failureReporter;
+	
 	@Override
 	public Auction auctionFor(Item item) {
-		//臨時null
-		//TODO XMPPAuction でItem を浮けとるようにする
-	    return new XMPPAuction(connection, item.identifier, null);
+	    return new XMPPAuction(connection, item.identifier, failureReporter);
 	}
 
 	public XMPPAuctionHouse(XMPPConnection connection) {
-		this.connection = connection;
+		this.connection      = connection;
+		this.failureReporter = new LoggingXMPPFailureReporter(makeLogger());
 	}
 
 	public static XMPPAuctionHouse connect(//
@@ -37,5 +43,24 @@ public class XMPPAuctionHouse implements AuctionHouse {
 
 	public void disconnect() {
 		connection.disconnect();		
+	}
+	private Logger makeLogger()  {
+		Logger logger = Logger.getLogger(LOGGER_NAME);
+		logger.setUseParentHandlers(false);
+		logger.addHandler(simpleFileHandler());
+		return logger;
+	}
+	private FileHandler simpleFileHandler() {
+		try {
+			FileHandler handler = new FileHandler(LOG_FILE_NAME);
+			handler.setFormatter(new SimpleFormatter());
+
+			return handler;
+		} catch (Exception e) {
+			//TODO 後で XMPPAuctionException を作る
+			throw new RuntimeException(
+					"Could not create logger FileHandler "
+							+ FilenameUtils.getFullPath(LOG_FILE_NAME), e);
+		}
 	}
 }
