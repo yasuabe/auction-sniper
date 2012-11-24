21217fb49e9a308d70669ceab60e6d85035f81be
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-24 14:38:31 +0900

    18.2: AuctionSniperDriver でも startBiddingWithStopPrice() の実装

--- auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ auction-sniper3/test/end-to-end/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -3,6 +3,8 @@ package test.endtoend.auctionsniper;
 import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
 import static com.objogate.wl.swing.matcher.IterableComponentsMatcher.matching;
 import static java.lang.String.valueOf;
+import static auctionsniper.ui.MainWindow.NEW_ITEM_ID_NAME;
+import static auctionsniper.ui.MainWindow.NEW_ITEM_STOP_PRICE_NAME;
 
 import javax.swing.JButton;
 import javax.swing.JTextField;
@@ -62,7 +64,15 @@ public class AuctionSniperDriver extends JFrameDriver {
 		return newItemId;
 	}
 	public void startBiddingWithStopPrice(String itemId, int stopPrice) {
-		// TODO Auto-generated method stub
-		
+	    textField(NEW_ITEM_ID_NAME).replaceAllText(itemId); 
+	    textField(NEW_ITEM_STOP_PRICE_NAME).replaceAllText(String.valueOf(stopPrice)); 
+	    bidButton().click(); 
+	}
+	private JTextFieldDriver textField(String fieldName) {
+		JTextFieldDriver newItemId = new JTextFieldDriver(this,
+				JTextField.class, named(fieldName));
+		newItemId.focusWithMouse();
+		newItemId.focusWithMouse();
+		return newItemId;
 	}
 }
