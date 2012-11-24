6ae18b66528c7d610fb37271bb999e643661b8a5
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-25 04:23:28 +0900

    SniperSnapshot に吸収されて不要になった SniperState を削除した

--- auction-sniper3/src/auctionsniper/AuctionEvent.java
+++ auction-sniper3/src/auctionsniper/AuctionEvent.java
@@ -14,19 +14,15 @@ public class AuctionEvent {
 	public String type() throws MissingValueException {
 		return get("Event");
 	}
-
 	public Price currentPrice() throws MissingValueException {
 		return Price.fromInt(getInt("CurrentPrice"));
 	}
-
 	public Increment increment() throws MissingValueException {
 		return Increment.fromInt(getInt("Increment"));
 	}
-
 	private int getInt(String fieldName) throws MissingValueException {
 		return Integer.parseInt(get(fieldName));
 	}
-	
 	public String get(String fieldName) throws MissingValueException {
 		String value = fields.get(fieldName);
 		if (null == value) {
@@ -34,7 +30,6 @@ public class AuctionEvent {
 		}
 		return value;
 	}
-
 	public static AuctionEvent from(String messageBody) {
 		AuctionEvent event = new AuctionEvent();
 		for (String field : fieldsIn(messageBody)) {
@@ -44,6 +39,7 @@ public class AuctionEvent {
 	}
 	private void addField(String field) {
 		String[] pair = field.split(":");
+		//TODO rule 5. One dot per line
 		fields.put(pair[0].trim(), pair[1].trim());
 	}
 	static String[] fieldsIn(String messageBody) {
@@ -52,7 +48,6 @@ public class AuctionEvent {
 	public PriceSource isFrom(String sniperId) throws MissingValueException {
 		return sniperId.equals(bidder()) ? FromSniper: FromOtherBidder;
 	}
-
 	private String bidder() throws MissingValueException {
 		return get("Bidder");
 	}
--- auction-sniper3/src/auctionsniper/SniperPortfolio.java
+++ auction-sniper3/src/auctionsniper/SniperPortfolio.java
@@ -6,6 +6,7 @@ import java.util.List;
 import auctionsniper.util.Announcer;
 
 public class SniperPortfolio implements SniperCollector {
+	//TODO rule 4. First class collections
 	private final Announcer<PortfolioListener> announcer = Announcer
 			.to(PortfolioListener.class);
 	private final List<AuctionSniper> snipers = new ArrayList<AuctionSniper>();
