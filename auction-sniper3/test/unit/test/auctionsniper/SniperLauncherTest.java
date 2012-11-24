754c2f0c1c27d20dd0290ce58a1970594270813b
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-24 13:56:51 +0900

    17.4: SniperLauncherTest の追加

---
+++ auction-sniper3/test/unit/test/auctionsniper/SniperLauncherTest.java
@@ -0,0 +1,52 @@
+package test.auctionsniper;
+
+import static org.hamcrest.Matchers.equalTo;
+
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.States;
+import org.junit.Test;
+
+import auctionsniper.Auction;
+import auctionsniper.AuctionHouse;
+import auctionsniper.AuctionSniper;
+import auctionsniper.SniperCollector;
+import auctionsniper.SniperLauncher;
+
+public class SniperLauncherTest {
+	private final Mockery context = new Mockery();
+	private final AuctionHouse auctionHouse = context.mock(AuctionHouse.class);
+	private final Auction auction = context.mock(Auction.class);
+	private final States auctionState = context.states("auction state");
+	private final SniperCollector sniperCollector = context.mock(SniperCollector.class);
+	private final SniperLauncher launcher = new SniperLauncher(auctionHouse,
+			sniperCollector);
+
+	@Test
+	public void addsNewSniperToCollectorAndThenJoinsAuction() {
+		final String itemId = "item 123";
+		context.checking(new Expectations() {{
+			allowing(auctionHouse).auctionFor(itemId); will(returnValue(auction));
+
+			oneOf(auction).addAuctionEventListener(with(sniperForItem(itemId)));
+				when(auctionState.isNot("not joined"));
+				
+			oneOf(sniperCollector).addSniper(with(sniperForItem(itemId)));
+				when(auctionState.isNot("not joined"));
+
+			oneOf(auction).join(); then(auctionState.is("joined"));
+		}});
+		launcher.joinAuction(itemId);
+	}
+	protected Matcher<AuctionSniper> sniperForItem(String itemId) {
+		return new FeatureMatcher<AuctionSniper, String>(
+				equalTo(itemId), "sniper with item id", "item") {
+			@Override
+			protected String featureValueOf(AuctionSniper actual) {
+				return actual.getSnapshot().itemId;
+			}
+		};
+	}
+}
