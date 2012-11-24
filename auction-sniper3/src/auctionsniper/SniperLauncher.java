ee29dfe19462ae3502d5ed5ff64fde36cc2ec3e9
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-24 13:23:53 +0900

    17.4: SniperLauncherの空実装追加

---
+++ auction-sniper3/src/auctionsniper/SniperLauncher.java
@@ -0,0 +1,11 @@
+package auctionsniper;
+
+public class SniperLauncher implements UserRequestListener {
+
+	@Override
+	public void joinAuction(String itemid) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
