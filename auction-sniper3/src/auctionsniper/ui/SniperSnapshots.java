c7434f4919ebfdba8e0d0b84cac1574c231d303b
Yasuyuki Abe <yasu@yasuabe.net>
2012-11-27 00:29:28 +0900

    rule 4: List<SniperSnapshot> を独立したクラスに

---
+++ auction-sniper3/src/auctionsniper/ui/SniperSnapshots.java
@@ -0,0 +1,31 @@
+package auctionsniper.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import auctionsniper.snapshot.SniperSnapshot;
+import auctionsniper.util.Defect;
+
+class SniperSnapshots {
+	private final List<SniperSnapshot> snapshots = new ArrayList<SniperSnapshot>();
+
+	public int add(SniperSnapshot snapshot) {
+		snapshots.add(snapshot);
+		return snapshots.size() - 1;
+	}
+	private int rowMatching(SniperSnapshot snapshot) {
+		for (int i = 0; i < snapshots.size(); i++) {
+			if (snapshot.isForSameItemAs(snapshots.get(i))) return i;
+		}
+		throw new Defect("Cannot find match for " + snapshot);
+	}
+	public int set(SniperSnapshot newSnapshot) {
+		int row = rowMatching(newSnapshot);
+		snapshots.set(row, newSnapshot);
+		return row;
+	}
+	public int size() { return snapshots.size(); }
+	public Object get(int row, Column column) { 
+		return column.valueIn(snapshots.get(row));
+	}
+}
--- auction-sniper3/src/auctionsniper/ui/SnipersTableModel.java
+++ auction-sniper3/src/auctionsniper/ui/SnipersTableModel.java
@@ -1,46 +1,35 @@
 package auctionsniper.ui;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.swing.table.AbstractTableModel;
 import auctionsniper.PortfolioListener;
 import auctionsniper.SniperListener;
 import auctionsniper.snapshot.SniperSnapshot;
 import auctionsniper.sniper.AuctionSniper;
-import auctionsniper.util.Defect;
 
 @SuppressWarnings("serial")
 public class SnipersTableModel extends AbstractTableModel implements
 		SniperListener, PortfolioListener {
 
-	//TODO rule 4. First class collections
-	private final List<SniperSnapshot> snapshots = new ArrayList<SniperSnapshot>();
+	SniperSnapshots snapshots = new SniperSnapshots();
 	
-	@Override public int getColumnCount() { return Column.values().length; }
+	@Override public int    getColumnCount()          { return Column.values().length; }
 	@Override public String getColumnName(int column) { return Column.at(column).name; }
-	@Override public int getRowCount() { return snapshots.size(); }
+
+	@Override public int    getRowCount() { return snapshots.size(); }
 	@Override public Object getValueAt(int row, int column) {
-		return Column.at(column).valueIn(snapshots.get(row));
+		return snapshots.get(row, Column.at(column));
 	}	
 
 	@Override public void sniperStateChanged(SniperSnapshot newSnapshot) {
-		int row = rowMatching(newSnapshot);
-		snapshots.set(row, newSnapshot);
+		int row = snapshots.set(newSnapshot);
 		fireTableRowsUpdated(row, row);
 	}
 	@Override public void sniperAdded(AuctionSniper sniper) {
 		sniper.register(this);
 		sniper.addSniperListener(new SwingThreadSniperListener(this));
 	}
-	private int rowMatching(SniperSnapshot snapshot) {
-		for (int i = 0; i < snapshots.size(); i++) {
-			if (snapshot.isForSameItemAs(snapshots.get(i))) return i;
-		}
-		throw new Defect("Cannot find match for " + snapshot);
-	}
 	public void addSniperSnapshot(SniperSnapshot snapshot) {
-		snapshots.add(snapshot);
-		int row = snapshots.size() - 1;
+		int row = snapshots.add(snapshot);
 		fireTableRowsInserted(row, row);
 	}
 }
